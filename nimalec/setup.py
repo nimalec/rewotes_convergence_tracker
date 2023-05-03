a2fafa8b4d8791aaa624707f649875488f28c8d6
nimalec <nl475@cornell.edu>
2023-05-03 14:15:42 -0400

    added files

---
+++ nimalec/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup( name='kconverge', version='0.1.0',author='Nima Leclerc',  author_email='nl475@cornell.edu', license='LICENSE.txt', description='Framework to determine optimal k-point mesh density in DFT calculation (via QuantumESPRESSO)')
