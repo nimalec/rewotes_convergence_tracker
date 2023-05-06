2e3b956c831c2acc9547c759acded5fe5d4f30e3
nimalec <nl475@cornell.edu>
2023-05-06 18:34:04 -0400

    byte

--- nimalec/kconverge/io.py
+++ nimalec/kconverge/io.py
@@ -25,8 +25,8 @@ def extract_job_id_submission(run_file='job.pbs'):
 
 def extract_run_status(job_id):
     output = subprocess.Popen("qstat ", shell=True, stdout=subprocess.PIPE).stdout.read()
-    print(job_id)
-    items=re.findall(job_id+".*$",output,re.MULTILINE)
+    print(job_id.type)
+    items=re.findall(str(job_id+".*$"),output,re.MULTILINE)
     if len(items) == 0:
         run_status = 'D'
     else:
