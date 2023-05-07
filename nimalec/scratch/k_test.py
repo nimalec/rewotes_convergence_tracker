a7b9612c13bff49b2d525ae340eb5290033c9b7a
nimalec <nl475@cornell.edu>
2023-05-07 15:15:33 -0400

    kconv

--- nimalec/scratch/k_test.py
+++ nimalec/scratch/k_test.py
@@ -6,7 +6,7 @@ threshold =  7.3498810939358E-7 ## threshold dE in Ry
 work_dir = './ktest_converge'
 
 ##SCF Parameters
-control = {'pseudo_dir': '../pseudo'}
+control = {'pseudo_dir': '../../pseudo'}
 pseudos = {'Si': 'si_pbe_gbrv_1.0.upf'}
 system = {'ecutwfc': 40, 'occupations': 'smearing', 'degauss': 0.005}
 electrons = {'mixing_beta': 0.3}
